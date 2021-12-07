6723bc077847c9d9412f4649f35b802f9a782cb1
kaisumi <kaisumi@langue-lab.com>
2021-12-07 14:18:37 +0900

    マジックナンバーを変数に格納、コメントを追加

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,19 +1,21 @@
 #!/usr/bin/env ruby
 
-# 定数の定義
+# 定数
 Three = 3
 Five = 5
-# Stringを宣言
-str_print = String.new
+Iteration = 20      # 繰り返し回数
 
-20.times do |count|
-    str_print = ""  # 初期化
-    count += 1      # 1から始める
-    if count % Three == 0 || count % Five == 0  # 3または5の倍数の時の処理
-        str_print = "Fizz" if count % 3 == 0    # 3の倍数なら必ず"Fizz"
-        str_print += "Buzz" if count % 5 == 0   # 5の倍数なら"Buzz"を追加
-    else                                
-        str_print = count.to_s                  # 3の倍数でも5の倍数でもなければ1から始まるcountを表示
+# 変数
+str_print = String.new  # Stringを宣言
+
+Iteration.times do |count|
+    str_print = ""                              # 初期化
+    count += 1                                  # 1から始める
+    if count % Three == 0 || count % Five == 0  # 3または5の倍数の場合
+        str_print = "Fizz" if count % 3 == 0        # 3の倍数なら必ず"Fizz"
+        str_print += "Buzz" if count % 5 == 0       # 5の倍数なら"Buzz"を追加
+    else                                        # 3の倍数でも5の倍数でもない場合
+        str_print = count.to_s                      # 1から始まるcountを表示
     end
     puts str_print                              # 出力
 end
